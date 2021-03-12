88c25e9d24fe431974109b8d7c44819911562af4
Caleb <sonicheze8@gmail.com>
2021-03-12 20:47:35 +0800

    Adding battle logic

---
+++ Assets/Scripts/BattleCore/Battle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using BattleCore;
+using UnityEngine;
+ 
+///summary
+///summary
+public class Battle : MonoBehaviour
+{
+ 
+    #region Public Fields
+
+    private List<BattleUnit> battleUnits;
+    private 
+    #endregion
+ 
+    #region Unity Methods
+ 
+    void Start()
+    {
+	
+    }
+ 
+    void Update()
+    {
+	
+    }
+ 
+    #endregion
+ 
+    #region Private Methods
+
+    public void BeginWar()
+    {
+	    
+    }
+    #endregion
+}
---
+++ Assets/Scripts/BattleCore/BattleUnit.cs
@@ -0,0 +1,24 @@
+﻿namespace BattleCore
+{
+    public abstract class BattleUnit
+    {
+        public Team team;
+        public BattleAction action;
+        
+    }
+
+    public abstract class BattleAction
+    {
+        public abstract void DoAction(BattleUnit unit, BattleMap map);
+
+    }
+
+    public class BattleMap
+    {
+    }
+
+    public class Team
+    {
+        public int teamIndex;
+    }
+}
